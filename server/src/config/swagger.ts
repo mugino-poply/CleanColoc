2bfe216d96c6694d9fe06f838d33622e3e2e546e
Hippolyte <hippolyte.amory@gmail.com>
2026-05-21 23:42:25 +0200

    fix: port 3000

--- server/src/config/swagger.ts
+++ server/src/config/swagger.ts
@@ -11,7 +11,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: "http://localhost:3001",
+                url: "http://localhost:3000",
                 description: "Serveur local de développement",
             },
         ],
